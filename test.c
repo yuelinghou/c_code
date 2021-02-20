a2160f90b1bdc620aa77d637cbd6fa36b4ed9b0c
yuelinghou <76507035+yuelinghou@users.noreply.github.com>
2021-02-21 00:23:42 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -2,242 +2,177 @@
 
 //#include<stdio.h>
 //
-//void bubble_sort(int* p, int n)
+//int main()
+//{
+//	printf("Hello World!\n");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	printf("Welcome to You!\n");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	printf("Programming in C is fun!\n");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
 //{
 //	int i = 0;
-//	for (i = 0; i < n - 1; i++)
+//	for (i = 0; i < 4; i++)
 //	{
 //		int j = 0;
-//		for (j = 0; j < n - 1 - i;j++)
+//		for (j = 0; j < 4 - i; j++)
 //		{
-//			if (p[j] > p[j + 1])
-//			{
-//				int tmp = p[j];
-//				p[j] = p[j + 1];
-//				p[j + 1] = tmp;
-//			}
+//			printf("*");
 //		}
-//	}
-//}
-//
-//int main()
-//{
-//	int arr[] = { 5,9,7,3,16,55,79,11,17,6 };
-//	int sz = sizeof(arr) / sizeof(arr[0]);
-//	bubble_sort(arr, sz);
-//	int i = 0;
-//	for (i = 0; i < sz; i++)
-//	{
-//		printf("%d ", arr[i]);
+//		printf("\n");
 //	}
 //	return 0;
 //}
+
 //#include<stdio.h>
-//
-//void init(int* p, int n)
-//{
-//	int i = 0;
-//	for (i = 0; i < n; i++)
-//	{
-//		*(p + i) = 0;
-//	}
-//}
 //
 //int main()
 //{
-//	int arr[] = {1,88,15,47,69,3,15,67};
-//	int sz = sizeof(arr) / sizeof(arr[0]);
-//	init(arr,sz);
+//	printf("  A\nA   A\n  A\n");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	printf("************\n  Welcome\n************\n");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	printf("What is a computer?\n");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
 //	int i = 0;
-//	for (i = 0; i < sz; i++)
+//	for (i = 0; i < 4; i++)
 //	{
-//		printf("%d ", arr[i]);
+//		int j = 0;
+//		for (j = 0; j < i; j++)
+//		{
+//			printf(" ");
+//		}
+//		for (j = 0; j < 4 - i; j++)
+//		{
+//			if (j != 4 - i - 1)
+//				printf("* ");
+//			else
+//				printf("*");
+//		}
+//		printf("\n");
 //	}
 //	return 0;
 //}
+
 //#include<stdio.h>
-//
-//void print(int* p, int n)
-//{
-//	int i = 0;
-//	for (i = 0; i < n; i++)
-//	{
-//		printf("%d ", *(p + i));
-//	}
-//}
 //
 //int main()
 //{
-//	int arr[] = { 1,88,15,47,69,3,15,67 };
-//	int sz = sizeof(arr) / sizeof(arr[0]);
-//	print(arr, sz);
+//	printf("fahr = 150, celsius = %d\n",5*(150-32)/9);
 //	return 0;
 //}
+
 //#include<stdio.h>
-//
-//void reverse(int* p, int n)
-//{
-//	int left = 0;
-//	int right = n - 1;
-//	while (left < right)
-//	{
-//		int tmp = p[left];
-//		p[left] = p[right];
-//		p[right] = tmp;
-//		left++;
-//		right--;
-//	}
-//}
 //
 //int main()
 //{
-//	int arr[] = { 1,88,15,47,69,3,15,67 };
-//	int sz = sizeof(arr) / sizeof(arr[0]);
-//	int i = 0;
-//	printf("逆序前：");
-//	for (i = 0; i < sz; i++)
-//	{
-//		printf("%-2d ", arr[i]);
-//	}
-//	printf("\n");
-//	reverse(arr, sz);
-//	printf("逆序后：");
-//	for (i = 0; i < sz; i++)
-//	{
-//		printf("%-2d ", arr[i]);
-//	}
+//	printf("height = %.2lf\n",0.5*10*3*3);
 //	return 0;
 //}
+
 //#include<stdio.h>
-//
-//void exchange(int* p1, int* p2, int n)
-//{
-//	int i = 0;
-//	for (i = 0; i < n; i++)
-//	{
-//		int tmp = *(p1 + i);
-//		*(p1 + i) = *(p2 + i);
-//		*(p2 + i) = tmp;
-//	}
-//}
 //
 //int main()
 //{
-//	int arr1[] = { 147,258,369 };
-//	int arr2[] = { 123,456,789 };
-//	int sz = sizeof(arr1) / sizeof(arr1[0]);
-//	//交换前的打印
-//	printf("交换前：\n");
-//	int i = 0;
-//	int j = 0;
-//	printf("arr1:");
-//	for (i = 0; i < sz; i++)
+//	printf("math = 87, eng = 72, comp = 93, average = %d", (87 + 72 + 93) / 3);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	printf("9 = 3 * 3\n3 * 3 = 9");
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	printf("celsius = 26, fahr = %d", 9 * 26 / 5 + 32);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	printf("152 = %d + %d*10 + %d*100\n", 152 % 10, 152 / 10 % 10, 152 / 100);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//
+//int main()
+//{
+//	double x = 0.0;
+//	scanf("%lf", &x);
+//	if (x)
 //	{
-//		printf("%d ", arr1[i]);
+//		printf("f(%.1lf) = %.1lf\n", x, 1 / x);
 //	}
-//	printf("\narr2:");
-//	for (j = 0; j < sz; j++)
+//	else
 //	{
-//		printf("%d ", arr2[j]);
-//	}
-//	//交换
-//	exchange(arr1, arr2, sz);
-//	//交换后的打印
-//	printf("\n交换后：\n");
-//	printf("arr1:");
-//	for (i = 0; i < sz; i++)
-//	{
-//		printf("%d ", arr1[i]);
-//	}
-//	printf("\narr2:");
-//	for (j = 0; j < sz; j++)
-//	{
-//		printf("%d ", arr2[j]);
+//		printf("f(0.0) = 0.0\n");
 //	}
 //	return 0;
 //}
+
 //#include<stdio.h>
-//
-//int count_bit_one(int num)
-//{
-//	int count = 0;
-//	while (num)
-//	{
-//		count++;
-//		num = num & (num - 1);
-//	}
-//	return count;
-//}
 //
 //int main()
 //{
-//	int num = 0;
-//	scanf("%d", &num);
-//	printf("%d\n", count_bit_one(num));
+//	int temporary = 0;
+//	scanf("%d", &temporary);
+//	printf("Celsius = %d\n", 5 * (temporary - 32) / 9);
 //	return 0;
 //}
+
 //#include<stdio.h>
-//
-//int count_diff_bit(int num1, int num2)
-//{
-//	int num = num1 ^ num2;
-//	int count = 0;
-//	while (num)
-//	{
-//		count++;
-//		num = num & (num - 1);
-//	}
-//	return count;
-//}
+//#include<math.h>
 //
 //int main()
 //{
-//	int num1 = 0;
-//	int num2 = 0;
-//	scanf("%d%d", &num1, &num2);
-//	printf("%d\n", count_diff_bit(num1, num2));
-//	return 0;
-//}
-//#include<stdio.h>
-//
-//void PrintBit(int num)
-//{
-//	printf("奇数位：");
-//	int i = 0;
-//	for (i = 30; i >= 0; i -= 2)
-//	{
-//		printf("%d ", (num >> i) & 1);
-//	}
-//	printf("\n偶数位：");
-//	for (i = 31; i >= 1; i -= 2)
-//	{
-//		printf("%d ", (num >> i) & 1);
-//	}
-//}
-//
-//int main()
-//{
-//	int num = 0;
-//	scanf("%d", &num);
-//	PrintBit(num);
-//	return 0;
-//}
-//#include<stdio.h>
-//
-//void exchange(int* num1, int* num2)
-//{
-//	*num1 = (*num1) ^ (*num2);
-//	*num2 = (*num1) ^ (*num2);
-//	*num1 = (*num1) ^ (*num2);
-//}
-//
-//int main()
-//{
-//	int num1 = 0;
-//	int num2 = 0;
-//	scanf("%d%d", &num1, &num2);
-//	printf("num1=%d num2=%d\n", num1, num2);
-//	exchange(&num1, &num2);
-//	printf("num1=%d num2=%d\n", num1, num2);
+//	double money = 0.0;
+//	double year = 0.0;
+//	double rate = 0.0;
+//	scanf("%lf%lf%lf", &money, &year, &rate);
+//	printf("interest = %.2lf\n", money*pow(1 + rate, year) - money);
 //	return 0;
 //}
